f60d5dc3558acac23fee1aface55cb78454977cf
Ryo inaba <vtryo.work@gmail.com>
2021-10-07 14:13:55 +0900

    補足を追加

--- ITP1_10_A/4.rb
+++ ITP1_10_A/4.rb
@@ -9,12 +9,12 @@ p3 = 0
 pp = (x[0] - y[0]).abs
 for i in 0...n do
   sum = (x[i] - y[i]).abs
-  p1 += sum
-  p2 += sum**2
+  p1 += sum    # 合計がマンハッタン距離
+  p2 += sum**2 # 合計の2乗がユークリッド距離
   p3 += sum**3
-  pp = sum if pp < sum
+  pp = sum if pp < sum # 最大値を取る
 end
 puts p1.round(6)
-puts sqrt(p2).round(6)
-puts (p3 ** (1.0/3.0)).round(6)
+puts sqrt(p2).round(6) # 累乗の平方根
+puts (p3 ** (1.0/3.0)).round(6) # (** 1/2)がp2なので、p3の場合は (** 1/3)
 puts pp.round(6)
